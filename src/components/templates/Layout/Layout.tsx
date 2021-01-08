5a6c301f9fefc206bd54809b1cfd1b382d988d28
Luiz Henrique <lpansarini@gmail.com>
2021-01-08 00:45:15 -0300

    fix(layout): insert header on layout and remove from home

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Head from 'next/head'
+import { Header } from '@/components/molecules'
 interface LayoutProps {
   children?: React.ReactNode
   title?: string
+  home?: boolean
 }
 
-const Layout: React.FC<LayoutProps> = ({ children, title = '' }) => {
+const Layout: React.FC<LayoutProps> = ({ children, title = '', home }) => {
   return (
-    <div className="antialiased text-gray-900 flex items-center justify-center min-h-screen">
+    <main className="max-h-screen">
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <main className="mx-auto max-w-5xl px-4 sm:px-6 md:px-8">
+      {!home && <Header />}
+      <section className="antialiased text-gray-900 flex flex-col items-center justify-center min-h-screen mx-auto max-w-5xl px-4 sm:px-6 md:px-8">
         {children}
-      </main>
-    </div>
+      </section>
+    </main>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,12 @@ import { Card } from '@/components/atoms'
 
 const Home: React.FC = () => {
   return (
-    <Layout title="Home - Contraktor">
+    <Layout title="Home - Contraktor" home>
       <h1 className="text-5xl mt-10 leading-none font-extrabold tracking-tight text-gray-900 sm:text-6xl lg:text-7xl md:mt-24">
         Contraktor CRUD
       </h1>
 
-      <div className="w-full grid grid-cols-1 grid-rows-2 gap-4 mt-10 md:grid-cols-2 md:grid-rows-1 md:mt-24">
+      <div className="w-2/3 grid grid-cols-1 grid-rows-2 gap-4 mt-10 md:grid-cols-2 md:grid-rows-1 md:mt-24">
         <Card href={'/contratos'} text="Contratos" />
         <Card href={'/partes'} text="Partes"/>
       </div>
